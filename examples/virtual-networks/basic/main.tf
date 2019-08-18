fee2052429060baed5309f6d93fa2d6892686b81
Tajjun <31044336+Tajjun@users.noreply.github.com>
2019-08-18 19:33:42 +0200

    Update main.tf

--- examples/virtual-networks/basic/main.tf
+++ examples/virtual-networks/basic/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "${var.location}"
+  name     = "taj-resources"
+  location = "westeurope"
 }
 
 resource "azurerm_virtual_network" "example" {
